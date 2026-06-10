17b0e900cf78594f896928d4bf2f038fdb7af94e
Samir Shaikh <shaikhsamir4325@gmail.com>
2026-06-10 13:53:57 +0530

    Update resource_monitor.sh

--- Task 5 - Shell-Scripting/resource_monitor.sh
+++ Task 5 - Shell-Scripting/resource_monitor.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 LOGFILE="resource_usage.log"
+EMAIL="samirsss777sss@gmail.com"
 
-echo "===== $(date) =====" >> $LOGFILE
+# CPU Usage
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
 
-echo "CPU Usage:" >> $LOGFILE
-top -bn1 | grep "Cpu(s)" >> $LOGFILE
+# Memory Usage
+MEMORY_USAGE=$(free -h | grep Mem)
 
-echo "Memory Usage:" >> $LOGFILE
-free -h >> $LOGFILE
+# Disk Usage (% used)
+DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-echo "Disk Usage:" >> $LOGFILE
-df -h / >> $LOGFILE
-
+echo "==================================" >> $LOGFILE
+echo "Date: $(date)" >> $LOGFILE
+echo "CPU Usage: $CPU_USAGE%" >> $LOGFILE
+echo "Memory Usage: $MEMORY_USAGE" >> $LOGFILE
+echo "Disk Usage: $DISK_USAGE%" >> $LOGFILE
+echo "==================================" >> $LOGFILE
 echo "" >> $LOGFILE
+
+# CPU Alert
+if [ "$CPU_USAGE" -gt 80 ]; then
+    echo "Warning: CPU Usage is $CPU_USAGE%" | mail -s "CPU Alert" $EMAIL
+fi
+
+# Disk Alert
+if [ "$DISK_USAGE" -gt 80 ]; then
+    echo "Warning: Disk Usage is $DISK_USAGE%" | mail -s "Disk Alert" $EMAIL
+fi
